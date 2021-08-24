d47db38d8b8b21d7978e641acd15bae78d3cdb0b
Francisco Arenas <arenas.frank00@gmail.com>
2021-08-24 15:47:55 -0500

    Updated main file

---
+++ sales_analysis_system/app.py
@@ -0,0 +1,9 @@
+'''
+Author: Francisco Arenas
+Creation Date: August 24th, 2021
+Last Modfication Date: August 2th, 2021
+
+Coding solution for the Lifestore case study of the first part of Emtech's 
+Data Science with Python course. To more information, consult the README.md 
+file in my github account: @farenasencis
+'''
